fcdcbf84cb0d3ddb6c60583a6be0767dedca7aaa
Darrennpoh <darrenpohchenghui06@gmail.com>
2026-08-06 19:56:48 +0800

    Fix Prisma Client for Production

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -3,10 +3,13 @@ import { PrismaPg } from '@prisma/adapter-pg'
 
 const globalForPrisma = globalThis
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL
-})
+function createPrismaClient() {
+  const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL
+  })
+  return new PrismaClient({ adapter })
+}
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
+export const prisma = globalForPrisma.prisma ?? createPrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
